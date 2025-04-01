adaa1806a0a197c99e88e8a94e5001c6a12e6f8a
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2025-04-01 21:37:23 +0100

    logo set

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -6,6 +6,7 @@ import {z} from "zod"
 import {Button} from "@/components/ui/button"
 import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form"
 import {Input} from "@/components/ui/input"
+import Image from "next/image";
 
 const formSchema = z.object({
     username: z.string().min(2).max(50),
@@ -25,7 +26,12 @@ const AuthForm = () => {
     }
 
     return (
-        <div>
+        <div className="card-border lg:min-w-[566px]">
+            <div className="flex flex-col gap-6 card py-14 px-10">
+                <div className="flex flex-row gap-2 justify-center">
+                    <Image src="/logo.svg" alt="logo" height={32} width={38}/>
+                </div>
+            </div>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                     <FormField
